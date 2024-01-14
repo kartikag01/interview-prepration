c9ff1ee55cb1328876228efe6401b54b881ed66c
kartikag01 <kartikagarwal01@gmail.com>
2024-01-14 13:56:02 +0530

    add deep clone also

---
+++ javascript/deep-clone.js
@@ -0,0 +1,22 @@
+/**
+ * @template T
+ * @param {T} value
+ * @return {T}
+ */
+export default function deepClone(value) {
+    if(!value) {
+      return value;
+    }
+    if(Array.isArray(value)) {
+      return value.map(_ => deepClone(_));
+    }
+    if(typeof value === "object") {
+      let mainObj = {};
+      console.log(value);
+      for(let [key, val] of Object.entries(value)) {
+        mainObj[key] = deepClone(val);
+      }
+      return mainObj;
+    }
+    return value;
+  }
